66e474d027e7c047f43ea7c05580a636e1a0afcf
Prachiti <vishangid@gmail.com>
2025-06-24 12:45:48 +0530

    Added : build a portfolio with spring boot and thymeleaf

---
+++ src/main/java/com/vishangidabir/newportfolio/NewportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.vishangidabir.newportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewportfolioApplication.class, args);
+	}
+
+}
